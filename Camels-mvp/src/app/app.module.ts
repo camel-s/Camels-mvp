0006419a95b04b6ea74fac33bdecff3aa28abddd
ArthurPlautz <tutuplautz@gmail.com>
2019-05-28 15:28:33 -0300

    SPA - Vamos começar a trabalhar

--- Camels-mvp/src/app/app.module.ts
+++ Camels-mvp/src/app/app.module.ts
@@ -8,13 +8,9 @@ import { FooterComponent } from './footer/footer.component';
 import { SideComponent } from './side/side.component';
 import { MenuMainComponent } from './menu/menu-main/menu-main.component';
 import { MenuInicialComponent } from './menu/menu-inicial/menu-inicial.component';
-import { ContentPerfilComponent } from './content/content-perfil/content-perfil.component';
 import { ContentContrateComponent } from './content/content-contrate/content-contrate.component';
 import { ContentOferecaComponent } from './content/content-ofereca/content-ofereca.component';
 import { SideLoginComponent } from './side/side-login/side-login.component';
-import { SideUsuarioComponent } from './side/side-usuario/side-usuario.component';
-import { SideOfertaComponent } from './side/side-oferta/side-oferta.component';
-import { SideAvaliacaoComponent } from './side/side-avaliacao/side-avaliacao.component';
 
 @NgModule({
   declarations: [
@@ -25,13 +21,9 @@ import { SideAvaliacaoComponent } from './side/side-avaliacao/side-avaliacao.com
     SideComponent,
     MenuMainComponent,
     MenuInicialComponent,
-    ContentPerfilComponent,
     ContentContrateComponent,
     ContentOferecaComponent,
-    SideLoginComponent,
-    SideUsuarioComponent,
-    SideOfertaComponent,
-    SideAvaliacaoComponent
+    SideLoginComponent
   ],
   imports: [
     BrowserModule
