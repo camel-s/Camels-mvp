9a267c96fae8caf38be82e638f0c89a5342d1896
ArthurPlautz <tutuplautz@gmail.com>
2019-05-29 17:20:23 -0300

    SPA - Components 04 - Routes 02

--- Camels-mvp/src/app/side/side.component.ts
+++ Camels-mvp/src/app/side/side.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideComponent implements OnInit {
 
-  constructor() { }
+  private display: number
+ 
+  constructor() { 
+     this.display = $(window).width();    
+  }
 
   ngOnInit() {
   }
